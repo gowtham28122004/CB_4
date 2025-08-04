49931454a89629ee8ea94862a0b328c0dc1badff
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2025-08-04 14:21:16 +0530

    Create Josephus.java

---
+++ Josephus Problem/Josephus.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Josephus
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int m = 0;
+		int i = 0;
+		while(Math.pow(2,i) < n) {
+		    m = (int)Math.pow(2,i++);
+		}
+		int r = n - m;
+		System.out.println(r*2+1);
+	}
+}
